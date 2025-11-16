bc1d59535260d5d3210c3efa45b2a084353ceba8
alexito0307 <alexito030710@gmail.com>
2025-11-16 15:48:54 -0600

    feat: :sparkles: Added JWT key for persistent login

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,31 @@
+// app/index.tsx
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 import { Redirect } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Index() {
-  return <Redirect href="/screens/login" />;
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("authToken");
+      console.log("TOKEN EN INDEX:", token);
+      setIsLoggedIn(!!token);
+      setChecking(false);
+    };
+
+    checkToken();
+  }, []);
+
+  if (checking) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return <Redirect href={isLoggedIn ? "/screens/feed/" : "/screens/login/"} />;
 }
