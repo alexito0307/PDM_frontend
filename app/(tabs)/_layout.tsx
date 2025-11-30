57d878f2ad6223df8d3d82780bb2f6fb9d376ad5
DerekRdgz <derek_rdgz@outlook.com>
2025-11-29 21:06:17 -0600

    feat: :sparkles: Added layout icons :D

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
@@ -11,12 +12,26 @@ export default function TabsLayout() {
         name="feed"
         options={{
           title: "Feed",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="home"
+              color={color}
+              size={size}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Perfil",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="person"
+              color={color}
+              size={size}
+            />
+          ),
         }}
       />
     </Tabs>
